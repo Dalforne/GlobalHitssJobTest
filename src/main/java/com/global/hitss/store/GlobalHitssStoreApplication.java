633c7dba1058515dee8956b3e81571ca5cbe98f1
Renato Dalforne <renato.dalforne@gmail.com>
2020-09-12 22:09:52 -0300

    ProjectStart

---
+++ src/main/java/com/global/hitss/store/GlobalHitssStoreApplication.java
@@ -0,0 +1,13 @@
+package com.global.hitss.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalHitssStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalHitssStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/hitss/store/GlobalHitssStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.hitss.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalHitssStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
